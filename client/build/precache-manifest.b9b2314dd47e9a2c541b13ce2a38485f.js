28d57aa6c8e5133ff0f95d1039ee9a5dd876fde9
Mihailob5017 <mihailob@gmail.com>
2020-08-06 23:41:07 +0200

    built

---
+++ client/build/precache-manifest.b9b2314dd47e9a2c541b13ce2a38485f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e392d5dc900eb2927b7f34ac68e4fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05e966605bc2a1bfdf2f",
+    "url": "/static/css/main.f5535440.chunk.css"
+  },
+  {
+    "revision": "3e8a92c6f5886f87e829",
+    "url": "/static/js/2.8f46aecf.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/static/js/2.8f46aecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05e966605bc2a1bfdf2f",
+    "url": "/static/js/main.97a6f742.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
